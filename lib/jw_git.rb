8bf2e8a1f49bc61e4a8f2f87c9c1cfa71b7fd211
Jelani Woods <jelani@firstdraft.com>
2020-03-24 17:45:00 -0500

    Maybe include date helper this way?

--- lib/jw_git.rb
+++ lib/jw_git.rb
@@ -7,10 +7,11 @@ module JwGit
   require "sinatra"
   require "date"
   require "git"
-  require 'action_view'
-  require 'action_view/helpers'
-  include ActionView::Helpers::DateHelper
   class Server < Sinatra::Base
+    require 'action_view'
+    require 'action_view/helpers'
+    include ActionView::Helpers::DateHelper
+    
     get '/' do
       working_dir = File.exist?(Dir.pwd + "/.git") ? Dir.pwd : Dir.pwd + "/.."
       g = Git.open(working_dir)
@@ -20,7 +21,7 @@ module JwGit
         # line = commit.sha + " " + commit.author.name + " " +
         # commit.date.strftime("%a, %d %b %Y, %H:%M %z") + " " + commit.message
         sha = commit.sha.slice(0..7)
-        commit_date = Date.parse commit.date
+        commit_date = commit.date
         line = " * " + sha + " - " + commit.date.strftime("%a, %d %b %Y, %H:%M %z") +
          " (#{time_ago_in_words(commit_date)} ago) " + "\n\t| " + commit.message 
         list.push line
